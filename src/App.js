21b0217ce2e5a47da45faca42b2fbae5d6925654
Manish <2706mkc@gmail.com>
2021-01-18 18:05:07 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import EditableTable from './Component/EditableTable';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to Antd Table</h1>
+     <EditableTable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/TableData.js
@@ -0,0 +1,54 @@
+const TableData ={
+    dataSource:[
+      {
+        key: '0',
+        id: '1000',
+        username: 'Mark@tcs.com',
+        fullname: 'Mark Johson',
+        age: '32',
+        address: 'London, Park Lane no. 0',
+      },
+      {
+        key: '1',
+        id: '1001',
+        username: 'Manish44@tcs.com',
+        fullname: 'Manish Chaudhary',
+        age: '23',
+        address: 'Cochin, Kerala',
+      },
+      {
+        key: '2',
+        id: '1002',
+        username: 'Mona@unisys.com',
+        fullname: 'Mona Minor',
+        age: '25',
+        address: 'Patna, Bihar',
+      },
+      {
+        key: '3',
+        id: '1003',
+        username: 'Mukund@oracle.com',
+        fullname: 'Mukund kumar',
+        age: '35',
+        address: 'London, Park Lane no. 2',
+      },
+      {
+          key: '4',
+          id: '1004',
+          username: 'Amit@tcs.com',
+          fullname: 'Amit Kumar',
+          age: '22',
+          address: 'Noida, UP',
+        },
+        {
+          key: '5',
+          id: '1005',
+          username: 'Kalai@tcs.com',
+          fullname: 'Kalivani',
+          age: '25',
+          address: 'JP Nagar Phase 5 Bangalore',
+        },
+    ],
+    count:6
+  };
+    export default TableData
